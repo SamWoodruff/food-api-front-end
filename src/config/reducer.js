d23ff8c758e74cb8f2f6f001c435e296c704c67b
tsmw2 <tsmw2@unigroupinc.com>
2019-07-17 15:28:29 -0500

    Initial Commit

---
+++ src/config/reducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    cart:[],
+    user:{}
+}
+
+export const reducer = (state=initialState,action) => {
+    switch(action.type){
+        default:
+            return state
+    }
+}
